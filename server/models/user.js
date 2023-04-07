f8cf554afcfa15788fac72157567246a5385d8db
Alt B 2022 <massie.w@gmail.com>
2023-04-07 13:06:47 -0700

    Added userData.json file with dummy data.  Modified seed.js to handle both insetion of locations as well as user seed data

--- server/models/user.js
+++ server/models/user.js
@@ -10,14 +10,31 @@ const userSchema = new Schema({
     type: String,
     required: true,
     unique: true,
-    // match: regex here
+    match: [/.+@.+\..+/, 'Must match an email address!'],
   },
   password: {
     type: String,
     required: true,
     unique: true,
+    minlength: 8,
   },
 });
+
+// set up pre-save middleware to create password
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
 
 const User = model('User', userSchema);
 
