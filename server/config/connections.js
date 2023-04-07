3796780b53412ea545c5d99db38ebcd9f286db39
Kaneknah <jamesnbaxley@protonmail.com>
2023-04-06 20:43:01 -0600

    wrote initial controllers code

--- server/config/connections.js
+++ server/config/connections.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect(
     //will need to check techmatchup and change it
-	process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/techmatchup",
+	process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/oceanPulse",
 	{
         //may need ot change some of this code
 		useNewUrlParser: true,
