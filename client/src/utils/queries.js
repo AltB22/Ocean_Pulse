b20fb5038bbe091e447cff5244dfb63c48553e1b
Kaneknah <jamesnbaxley@protonmail.com>
2023-04-07 16:46:02 -0600

    Created queiries for Get Locations, add location, comment, remove comment

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,24 +1,97 @@
 import { gql } from "@apollo/client";
 
+//GETs a list of locations
+export const GET_LOCATIONS = gql`
+	query GetLocations {
+		locations {
+			_id
+			surfSpot
+			location
+			type
+			optimalSwellDirection
+			optimal_wind
+			optimalSwellSize
+			optimalTide
+			comments {
+				_id
+				comment
+			}
+		}
+	}
+`;
 
-// this will need to be edited to fit our code TECH and MATCHUPS are our to be replaced by our models
-// export const QUERY_TECH = gql`
-// 	query tech {
-// 		tech {
-// 			_id
-// 			name
-// 		}
-// 	}
-// `;
+// Adds a new location
+export const ADD_LOCATION = gql`
+	mutation AddLocation(
+		$surfSpot: String!
+		$location: String!
+		$type: String!
+		$optimalSwellDirection: Int!
+		$optimal_wind: Int!
+		$optimalSwellSize: Int!
+		$optimalTide: String!
+	) {
+		addLocation(
+			surfSpot: $surfSpot
+			location: $location
+			type: $type
+			optimalSwellDirection: $optimalSwellDirection
+			optimal_wind: $optimal_wind
+			optimalSwellSize: $optimalSwellSize
+			optimalTide: $optimalTide
+		) {
+			_id
+			surfSpot
+			location
+			type
+			optimalSwellDirection
+			optimal_wind
+			optimalSwellSize
+			optimalTide
+			comments {
+				_id
+				comment
+			}
+		}
+	}
+`;
 
-// export const QUERY_MATCHUPS = gql`
-// 	query matchups($_id: String) {
-// 		matchups(_id: $_id) {
-// 			_id
-// 			tech1
-// 			tech2
-// 			tech1_votes
-// 			tech2_votes
-// 		}
-// 	}
-// `;
+//Adds a new comment to a location
+export const ADD_COMMENT = gql`
+	mutation AddComment($locationId: ID!, $comment: String!) {
+		addComment(locationId: $locationId, comment: $comment) {
+			_id
+			surfSpot
+			location
+			type
+			optimalSwellDirection
+			optimal_wind
+			optimalSwellSize
+			optimalTide
+			comments {
+				_id
+				comment
+			}
+		}
+	}
+`;
+
+//Removes a comment from a specific location
+export const REMOVE_COMMENT = gql`
+	mutation RemoveComment($locationId: ID!, $commentId: ID!) {
+		removeComment(locationId: $locationId, commentId: $commentId) {
+			_id
+			surfSpot
+			location
+			type
+			optimalSwellDirection
+			optimal_wind
+			optimalSwellSize
+			optimalTide
+			comments {
+				_id
+				comment
+			}
+		}
+	}
+`;
