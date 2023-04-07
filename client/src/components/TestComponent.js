8b74363f0f36c83b39c13d4f6e5e3b7aeb9f8838
Vinicius <Zizoocz@gmail.com>
2023-04-06 20:16:22 -0700

    added package.json and some testing code

---
+++ client/src/components/TestComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TestComponent = () => {
+    return (
+        <header class="row-1">
+            <h1>Surf Report</h1>
+        </header>
+
+    )
+}
+
+export default TestComponent
+
