dfd52ebff3aa8fa84947083a7b2a7e6f16239ede
Alt B 2022 <massie.w@gmail.com>
2023-04-10 23:27:40 -0700

    Added authMiddleware to server.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,18 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
+
+console.log("descriptive console log",process.env)
+
 
 mongoose.connect(
-	process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/oceanPulse",
+	// process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/oceanPulse",
+	process.env.REACT_APP_ATLAS_URI || "mongodb://127.0.0.1:27017/oceanPulse",
 	{
         //may need to change some of this code
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
+		// useCreateIndex: true,
+		// useFindAndModify: false
 	}
 );
 
