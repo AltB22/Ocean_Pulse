9fb1e8814c15aa1e353c7343bb2fd1c1657f1ed4
Kaneknah <jamesnbaxley@protonmail.com>
2023-04-13 21:54:10 -0600

    commented out login

--- client/src/components/pages/login.js
+++ client/src/components/pages/login.js
@@ -1,77 +1,77 @@
-// import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../../utils/mutations';
-import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
+// // import { Link } from 'react-router-dom';
+// import { useMutation } from '@apollo/client';
+// import { LOGIN_USER } from '../../utils/mutations';
+// import React, { useState } from "react";
+// import { Form, Button } from "react-bootstrap";
 
-import Auth from '../../utils/auth';
+// import Auth from '../../utils/auth';
 
-const Login = (props) => {
-    const [formState, setFormState] = useState({ email: '', password: '' });
-    const [login, { error, data }] = useMutation(LOGIN_USER);
+// const Login = (props) => {
+//     const [formState, setFormState] = useState({ email: '', password: '' });
+//     const [login, { error, data }] = useMutation(LOGIN_USER);
 
-    // update state based on form input changes
-    const handleChange = (event) => {
-        const { name, value } = event.target;
+//     // update state based on form input changes
+//     const handleChange = (event) => {
+//         const { name, value } = event.target;
 
-        setFormState({
-            ...formState,
-            [name]: value,
-        });
-    };
+//         setFormState({
+//             ...formState,
+//             [name]: value,
+//         });
+//     };
 
-    // submit form
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
-        console.log(formState);
-        try {
-            const { data } = await login({
-                variables: { ...formState },
-            });
+//     // submit form
+//     const handleFormSubmit = async (event) => {
+//         event.preventDefault();
+//         console.log(formState);
+//         try {
+//             const { data } = await login({
+//                 variables: { ...formState },
+//             });
 
-            Auth.login(data.login.token);
-        } catch (e) {
-            console.error(e);
-        }
+//             Auth.login(data.login.token);
+//         } catch (e) {
+//             console.error(e);
+//         }
 
-        // clear form values
-        setFormState({
-            email: '',
-            password: '',
-        });
-    };
+//         // clear form values
+//         setFormState({
+//             email: '',
+//             password: '',
+//         });
+//     };
 
-    return (
-        <Form onSubmit={handleFormSubmit}>
-            <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                    type="email"
-                    name='email'
-                    placeholder="Enter email"
-                    value={formState.email}
-                    onChange={handleChange}
-                    required
-                />
-            </Form.Group>
+//     return (
+//         <Form onSubmit={handleFormSubmit}>
+//             <Form.Group controlId="formBasicEmail">
+//                 <Form.Label>Email address</Form.Label>
+//                 <Form.Control
+//                     type="email"
+//                     name='email'
+//                     placeholder="Enter email"
+//                     value={formState.email}
+//                     onChange={handleChange}
+//                     required
+//                 />
+//             </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                    type="password"
-                    name='password'
-                    placeholder="Password"
-                    value={formState.password}
-                    onChange={handleChange}
-                    required
-                />
-            </Form.Group>
+//             <Form.Group controlId="formBasicPassword">
+//                 <Form.Label>Password</Form.Label>
+//                 <Form.Control
+//                     type="password"
+//                     name='password'
+//                     placeholder="Password"
+//                     value={formState.password}
+//                     onChange={handleChange}
+//                     required
+//                 />
+//             </Form.Group>
 
-            <Button variant="primary" type="submit">
-                Log In
-            </Button>
-        </Form>
-    );
-}
+//             <Button variant="primary" type="submit">
+//                 Log In
+//             </Button>
+//         </Form>
+//     );
+// }
 
-export default Login;
+// export default Login;
