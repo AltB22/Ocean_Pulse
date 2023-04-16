98f888feaa811e6bef208531b04a8981ff936cfc
Alt B 2022 <massie.w@gmail.com>
2023-04-16 01:57:44 -0700

    Fixed infinite loop in Locations.js. I hit my api call limit immediately and had to upgrade the plan

--- client/src/components/pages/Locations copy.js
+++ client/src/components/pages/Locations copy.js
@@ -17,6 +17,15 @@ const styles = {
     },
 };
 
+const GetCurrentConditions = (selectedSpot) => {
+  switch (selectedSpot.surf_spot) {
+    case 'Ocean Beach':
+      GetSurfReport(selectedSpot.surf_spot, 37.75545, -122.5292);
+     break;
+    default:
+      return 'No current conditions available.';
+  }
+};
 
 async function GetSurfReport(selectedSpot,lat,lng) {//function that accepts the 3 parameters from above and fetches data based on their values (lat & lng are the functionals here) plus other defined vars params & source
  
@@ -34,62 +43,57 @@ async function GetSurfReport(selectedSpot,lat,lng) {//function that accepts the 
 
 const surfReport = await response.json();//defining api response as json object
 console.log(surfReport)
-//  const surfReportArr = [surfReport]
+ const surfReportArr = [surfReport]
 
   return;
 
+  
+ 
+  
   // console.log(surfReportArr)
   // // currentSurfSpot = surfSpot; // Assign the value of surfSpot to currentSurfSpot
   // // setCurrentConditions(surfReport);//passes data to renderSurfForecast
   // return surfReportArr;
 
 }
-
-
 const Locations = () => {
-  const { loading, data } = useQuery(GET_LOCATIONS);
+    const { loading, data, surfReport } = useQuery(GET_LOCATIONS);
 
-  const locationData = data?.locations || [];
-  const [selectedSpot, setSelectedSpot] = useState(null);
-  const [currentConditions, setCurrentConditions] = useState(null);
+    const locationData = data?.locations || [];
+    const [selectedSpot, setSelectedSpot] = useState(null);
+    const [currentConditions, setCurrentConditions] = useState(null);
+    
+    const handleDropdownSelect = (eventKey, spot) => {
+    
+     setSelectedSpot(locationData[eventKey]);
+     setCurrentConditions(surfReport[spot]);
+     console.log(currentConditions)
+      // setCurrentConditions([locationData.city]);
+    
+    
+  }
 
-  useEffect(() => {
-    const getCurrentConditions = async () => {
-      switch (selectedSpot?.surf_spot) {
-        case 'Ocean Beach':
-          const surfReport = await GetSurfReport(selectedSpot.surf_spot, 37.75545, -122.5292);
-          setCurrentConditions(surfReport);
-          break;
-        default:
-          setCurrentConditions('No current conditions available.');
-      }
-    };
+    return (
 
-    getCurrentConditions();
-  }, [selectedSpot]);
+        <div className="Location">
+            {/* create a dropdown in jsx with the data from locationData.surf_spot */}
+            <Dropdown onSelect={handleDropdownSelect}>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    Search Locations
+                </Dropdown.Toggle>
+                {/* {data && data.location && data.location.surf_spot && <h2>{data.location.surf_spot}</h2>} */}
 
-  const handleDropdownSelect = (eventKey) => {
-    setSelectedSpot(locationData[eventKey]);
-  };
-
-  return (
-    <div className="Location">
-      <Dropdown onSelect={handleDropdownSelect}>
-        <Dropdown.Toggle variant="success" id="dropdown-basic">
-          Search Locations
-        </Dropdown.Toggle>
-        <Dropdown.Menu>
-          {locationData.map((spot, i) => (
-            <Dropdown.Item href="#/action-1" key={i} eventKey={i}>
-              {spot?.surf_spot}
-            </Dropdown.Item>
-          ))}
-        </Dropdown.Menu>
-      </Dropdown>
-
-      <h1 style={styles.heading}>{selectedSpot?.surf_spot}</h1>
-
-      {selectedSpot && (
+                <Dropdown.Menu>
+                   {locationData.map((spot, i)=>(
+                     <Dropdown.Item href="#/action-1" key={i} eventKey={i}>
+                        {spot?.surf_spot}
+                     </Dropdown.Item>
+                   ))}
+                </Dropdown.Menu>
+            </Dropdown>
+            
+            <h1 style={styles.heading} >{selectedSpot?.surf_spot}</h1>
+            {selectedSpot && (
         <Row xs={1} md={2} className="g-4">
           <Col>
             <Card className="AboutLocation">
@@ -100,6 +104,7 @@ const Locations = () => {
                 <Card.Text>Best Swell Direction(s): {selectedSpot.optimal_swell_direction}</Card.Text>
                 <Card.Text>Best Wind Direction: {selectedSpot.optimal_wind}</Card.Text>
                 <Card.Text>Best Tide: {selectedSpot.optimal_tide}</Card.Text>
+               
               </Card.Body>
             </Card>
           </Col>
@@ -107,7 +112,9 @@ const Locations = () => {
             <Card className="CurrentConditions">
               <Card.Body>
                 <Card.Title>Current Swell & Wind Conditions for {[selectedSpot.surf_spot]}</Card.Title>
-                <Card.Text>{currentConditions}</Card.Text>
+                <Card.Text>{setCurrentConditions(selectedSpot)}</Card.Text>
+                <Card.Text> {GetCurrentConditions(selectedSpot)}</Card.Text>
+                <Card.Text> {currentConditions}</Card.Text>
               </Card.Body>
             </Card>
           </Col>
@@ -115,6 +122,6 @@ const Locations = () => {
       )}
     </div>
   );
-};
+            }
          
 export default Locations;
