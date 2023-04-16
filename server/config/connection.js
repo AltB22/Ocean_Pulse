de775a9284bf2ebc6d8c9a7105d816b153ea32c3
Alt B 2022 <massie.w@gmail.com>
2023-04-16 00:08:51 -0700

    Dropped in code from test ocean pulse functions and have gotten the dropdown component location name selected as a prop passed into the switch case and fetching stormglass api data.  Successful console log of the data

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
-// require('dotenv').config({ path: __dirname+"/../../.env" });//comment in when seeding data
+require('dotenv').config({ path: __dirname+"/../../.env" });//comment in when seeding data
 
 
 
 
 mongoose.connect(
-	process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/oceanPulse",
+	process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/ocean_pulse",
 
 	{
 		//may need to change some of this code
