bf3a7fbce9d008edc5084ab9954bf1aec2fe1b58
Vinicius <Zizoocz@gmail.com>
2023-04-17 17:18:21 -0700

    added some changes

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -3,40 +3,37 @@ import decode from "jwt-decode";
 // const decode = require("jwt-decode");
 
 class AuthService {
-	//Finds user with the decoded JWT token for the local storage
-	getUser() {
-		return decode(this.getToken());
-	}
-	//Checks to see if a token is present
-	loggedIn() {
-		const token = this.getToken();
-		return token && !this.isTokenExpired(token) ? true : false;
-	}
-	//checks for expired token
-	isTokenExpired(token) {
-		const decoded = decode(token);
-		if (decoded.exp < Date.now() / 1000) {
-			localStorage.removeItem("id_token");
-			return true;
-		}
-		return false;
-	}
+  getProfile() {
+    return decode(this.getToken());
+  }
 
-	//Gets the token from local storage
-	getToken() {
-		return localStorage.getItem("id_token");
-	}
+  loggedIn() {
+    const token = this.getToken();
+    return token && !this.isTokenExpired(token) ? true : false;
+  }
 
-	//Stores the token in local storage
-	login(idToken) {
-		localStorage.setItem("id_token", idToken);
-		window.location.assign("/");
-	}
-	//Removes the Token from local storage and reloads the page.
-	logout() {
-		localStorage.removeItem("id_token");
-		window.location.reload();
-	}
+  isTokenExpired(token) {
+    const decoded = decode(token);
+    if (decoded.exp < Date.now() / 1000) {
+      localStorage.removeItem('id_token');
+      return true;
+    }
+    return false;
+  }
+
+  getToken() {
+    return localStorage.getItem('id_token');
+  }
+
+  login(idToken) {
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  }
+
+  logout() {
+    localStorage.removeItem('id_token');
+    window.location.reload();
+  }
 }
 
 export default new AuthService();
--- server/config/connection.js
+++ server/config/connection.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 
 
 mongoose.connect(
-	process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/oceanPulse",
+	process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/ocean_pulse",
 
 	{
 		//may need to change some of this code
